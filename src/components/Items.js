b0aefa179fa204e9d43e05a0567c8e907d85d230
3jun <kk3june@gmail.com>
2023-01-10 20:16:57 +0900

    feat. setState 메소드 구현

---
+++ src/components/Items.js
@@ -0,0 +1,33 @@
+import Component from '../core/Components.js';
+
+export default class Items extends Component {
+  setup() {
+    this.$state = { items: ['item1', 'item2', 'item3', 'item4'] };
+  }
+
+  template() {
+    const { items } = this.$state;
+    return `
+          <ul>
+            ${items
+              .map(
+                (item, key) => `
+              <li>
+                ${item}
+                <button class="deleteBtn" data-index="${key}">삭제</button>
+              </li>
+            `,
+              )
+              .join('')}
+          </ul>
+          <button>추가</button>
+        `;
+  }
+
+  setEvent() {
+    this.$target.querySelector('button').addEventListener('click', () => {
+      const { items } = this.$state;
+      this.setState({ items: [...items, `item${items.length + 1}`] });
+    });
+  }
+}
