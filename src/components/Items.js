06394e04617e61782e9d4386acac67f4e1716878
3jun <kk3june@gmail.com>
2023-01-11 13:46:15 +0900

    feat. 삭제 기능 구현

--- src/components/Items.js
+++ src/components/Items.js
@@ -29,5 +29,13 @@ export default class Items extends Component {
       const { items } = this.$state;
       this.setState({ items: [...items, `item${items.length + 1}`] });
     });
+
+    this.$target.querySelectorAll('.deleteBtn').forEach((deleteBtn) =>
+      deleteBtn.addEventListener('click', ({ target }) => {
+        const items = [...this.$state.items];
+        items.splice(target.dataset.index, 1);
+        this.setState({ items });
+      }),
+    );
   }
 }
